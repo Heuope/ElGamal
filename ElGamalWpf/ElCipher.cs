0bd8d8f8ea34795c7d366d6f2834de74f92bb8a1
Kostya <konstantin.sedniov@gmail.com>
2019-11-11 22:48:18 +0300

    first commit

---
+++ ElGamalWpf/ElCipher.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ElGamalWpf
+{
+    class ElCipher
+    {
+        private readonly string _saveFilePath;
+        private readonly string _loadFilePath;
+
+        public ElCipher(string loadFilePath, string saveFilePath)
+        {
+            _loadFilePath = loadFilePath;
+            _saveFilePath = saveFilePath;
+        }
+
+        public bool TryCipher(BigInteger p, BigInteger x, BigInteger k, BigInteger g)
+        {
+            if (string.IsNullOrEmpty(_saveFilePath) || string.IsNullOrEmpty(_loadFilePath))
+                return false;
+            if (!Ferma(p))
+                return false;
+            if (x >= p || k >= p || x < 1 || k < 1)
+                return false;
+            if (Gcd(k, p - 1) != 1)
+                return false;
+            if (FastPow(g, (p - 1) / 2, p) == 1)
+                return false;
+            if (FastPow(g, (p - 1) / 3, p) == 1)
+                return false;
+
+            Cipher(p, x, k, g);
+            return true;
+        }
+
+        public bool TryDeCipher(BigInteger p, BigInteger x)
+        {
+            if (string.IsNullOrEmpty(_saveFilePath) || string.IsNullOrEmpty(_loadFilePath))
+                return false;
+            if (!Ferma(p))
+                return false;
+            if (x >= p || x < 1)
+                return false;
+
+            DeCipher(p, x);
+            return true;
+        }
+
+        private BigInteger FastPow(BigInteger number, BigInteger power, BigInteger modul)
+        {
+            BigInteger x = 1;
+            while (power != 0)
+            {
+                while (power % 2 == 0)
+                {
+                    power /= 2;
+                    number = number * number % modul;
+                }
+                power--;
+                x = (x * number) % modul;
+            }
+            return x;
+        }
+
+        private BigInteger Gcd(BigInteger a, BigInteger b)
+        {
+            if (b == 0)
+                return a;
+            return Gcd(b, a % b);
+        }
+
+        private bool Ferma(BigInteger x)
+        {
+            if (x == 2)
+                return true;
+
+            var rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                BigInteger a = (rand.Next() % (x - 2)) + 2;
+                if (Gcd(a, x) != 1)
+                    return false;
+                if (FastPow(a, x - 1, x) != 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private void DeCipher(BigInteger p, BigInteger x)
+        {
+            var initFile = System.IO.File.ReadAllBytes(_loadFilePath);
+            var resFile = new List<byte>();
+
+            for (var i = 0; i < initFile.Length; i += 2)
+                Console.WriteLine((FastPow(initFile[i], p - 1 - x, p) * initFile[i + 1]) % p);
+
+            System.IO.File.WriteAllBytes(_saveFilePath, resFile.ToArray());
+        }
+
+        private void Cipher(BigInteger p, BigInteger x, BigInteger k, BigInteger g)
+        {
+            
+
+            for (int i = 2; i < p; i++)
+                if (Gcd(i, p - 1) == 1)
+                    Console.Write($"{i} ");
+
+            var initFile = System.IO.File.ReadAllBytes(_loadFilePath);
+            var resFile = new List<BigInteger>();
+
+            var y = FastPow(g, x, p);
+            var a = FastPow(g, k, p);
+
+            foreach (var m in initFile)
+            {
+                resFile.Add(a);
+                resFile.Add((FastPow(y, k, p) * m) % p);
+            }
+
+            //System.IO.File.WriteAllBytes(_saveFilePath, resFile.ToArray());
+        }
+    }
+}
---
+++ ElGamalWpf/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElGamalWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _loadFilePath;
+        private string _saveFilePath;
+        private object bigintger;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _loadFilePath = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                _saveFilePath = saveFileDialog.FileName;
+            }
+        }
+
+        private void Check(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(p.Text))
+            {
+                g.Items.Clear();
+                return;
+            }
+            BigInteger tempP = BigInteger.Parse(p.Text);
+
+            for (var item = 1; item < tempP - 1; item++)
+                if (FastPow(item, (tempP - 1) / 2, p) != 1)
+                    if (FastPow(item, (tempP - 1) / 3, p) != 1)
+                        Console.Write($"{item} ");
+
+            if (string.IsNullOrEmpty(g.SelectedItem.ToString()))
+                return;
+            if (string.IsNullOrEmpty(x.Text) || string.IsNullOrEmpty(k.Text))
+                return;
+
+            Cipher.IsEnabled = true;
+        }
+    }
+}
